1de05b0dfaee428abe6ab0e4c71475b7aead2f97
xleMnlx <eman.darca86@gmail.com>
2023-06-14 10:05:54 +0800

    Add project files.

---
+++ Timer/Form1.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.button_start = New System.Windows.Forms.Button()
+        Me.button_exit = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.button_reset = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label1.Location = New System.Drawing.Point(78, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(32, 23)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "00"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label2.Location = New System.Drawing.Point(127, 18)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(28, 21)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "00"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label3.Location = New System.Drawing.Point(179, 18)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(25, 19)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "00"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label4.Location = New System.Drawing.Point(60, 19)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(12, 19)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = ":"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label5.Location = New System.Drawing.Point(161, 16)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(12, 19)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = ":"
+        '
+        'button_start
+        '
+        Me.button_start.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.button_start.Font = New System.Drawing.Font("Times New Roman", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.button_start.Location = New System.Drawing.Point(35, 43)
+        Me.button_start.Name = "button_start"
+        Me.button_start.Size = New System.Drawing.Size(75, 23)
+        Me.button_start.TabIndex = 5
+        Me.button_start.Text = "Start / Stop"
+        Me.button_start.UseVisualStyleBackColor = True
+        '
+        'button_exit
+        '
+        Me.button_exit.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.button_exit.Font = New System.Drawing.Font("Times New Roman", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.button_exit.Location = New System.Drawing.Point(116, 43)
+        Me.button_exit.Name = "button_exit"
+        Me.button_exit.Size = New System.Drawing.Size(75, 52)
+        Me.button_exit.TabIndex = 6
+        Me.button_exit.Text = "Exit"
+        Me.button_exit.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 10
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Times New Roman", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label6.Location = New System.Drawing.Point(12, 9)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(42, 31)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "00"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label7.Location = New System.Drawing.Point(109, 16)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(12, 19)
+        Me.Label7.TabIndex = 8
+        Me.Label7.Text = ":"
+        '
+        'button_reset
+        '
+        Me.button_reset.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.button_reset.Font = New System.Drawing.Font("Times New Roman", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.button_reset.Location = New System.Drawing.Point(35, 72)
+        Me.button_reset.Name = "button_reset"
+        Me.button_reset.Size = New System.Drawing.Size(75, 23)
+        Me.button_reset.TabIndex = 9
+        Me.button_reset.Text = "Start / Stop"
+        Me.button_reset.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Purple
+        Me.ClientSize = New System.Drawing.Size(216, 125)
+        Me.Controls.Add(Me.button_reset)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.button_exit)
+        Me.Controls.Add(Me.button_start)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Timer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents button_start As System.Windows.Forms.Button
+    Friend WithEvents button_exit As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents button_reset As Button
+End Class
---
+++ Timer/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+
+    Private Sub button_exit_Click(sender As Object, e As EventArgs) Handles button_exit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub button_start_Click(sender As Object, e As EventArgs) Handles button_start.Click
+        If Timer1.Enabled = False Then
+            Timer1.Enabled = True
+        Else
+            Timer1.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label3.Text = Label3.Text + 1
+        If Label3.Text = "60" Then
+            Label2.Text = Label2.Text + 1
+            Label3.Text = "00"
+        ElseIf Label2.Text = "60" Then
+            Label1.Text = Label1.Text + 1
+            Label2.Text = "00"
+        ElseIf Label1.Text = "60" Then
+            Label6.Text = Label6.Text + 1
+            Label1.Text = "00"
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Enabled = False
+    End Sub
+
+    Private Sub button_reset_Click(sender As Object, e As EventArgs) Handles button_reset.Click
+        Label3.Text = "00"
+        Label2.Text = "00"
+        Label1.Text = "00"
+        Label6.Text = "00"
+    End Sub
+End Class
